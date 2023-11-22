781637ef8078dcbfc0550f82b24df0bb8d8d7e1e
maloneykhim <maloneykhim@gmail.com>
2023-11-22 18:41:08 -0500

    newpageeditmeme

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,36 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleButton = document.getElementById("darkmode-toggle");
+  let isAnimating = false; // Flag to track the animation state
+
+  toggleButton.addEventListener("click", function () {
+    if (isAnimating) {
+      return; // If the animation is in progress, do nothing
+    }
+
+    isAnimating = true; // Set the animation flag
+
+    // Delay the page change after the animation is complete
+    setTimeout(function () {
+      if (window.location.href.includes("editmemelight.html")) {
+        // If currently on page1, navigate to page2
+        window.location.href = "editmemedark.html";
+      } else if (window.location.href.includes("editmemedark.html")) {
+        // If currently on page2, navigate back to page1
+        window.location.href = "editmemelight.html";
+      }
+
+      isAnimating = false; // Reset the animation flag after the page change
+    }, 1000); // Adjust the delay time (in milliseconds) as needed
+  });
+});
+
+
+
+
+
+
+
+
 document.addEventListener('DOMContentLoaded', function () {
   const imageContainer = document.querySelector('.image-container');
   const colorPicker = document.getElementById('colorPicker');
